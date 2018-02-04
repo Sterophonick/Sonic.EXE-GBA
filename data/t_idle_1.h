16ad11bad48cc0c87f4cee5c9eb2a7f0d1922adc
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2018-02-04 13:08:46 -0700

    Add files via upload

---
+++ data/t_idle_1.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(t_idle_1)
+
+//======================================================================
+//
+//	t_idle_1, 64x64@4, 
+//	+ palette 16 entries, not compressed
+//	+ 64 tiles not compressed
+//	Total size: 32 + 2048 = 2080
+//
+//	Time-stamp: 2018-02-01, 11:16:03
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_T_IDLE_1_H
+#define GRIT_T_IDLE_1_H
+
+#define t_idle_1TilesLen 2048
+extern const unsigned short t_idle_1Tiles[1024];
+
+#define t_idle_1PalLen 32
+extern const unsigned short t_idle_1Pal[16];
+
+#endif // GRIT_T_IDLE_1_H
+
+//}}BLOCK(t_idle_1)
